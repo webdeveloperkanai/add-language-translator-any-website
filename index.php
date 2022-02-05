f0b54b4aaf35102a6c4c4b2b10137d26bb14c5f3
Kanai Shil <70555095+webdeveloperkanai@users.noreply.github.com>
2022-02-05 17:36:31 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<div class="select-container select-container-sm" id="google_translate_element"></div>
+<script type="text/javascript">
+  function googleTranslateElementInit() {
+	   new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+	}
+</script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+<style>
+#\:0\.targetLanguage .goog-te-combo {
+    padding: 10px; 
+    background: white;
+    border-radius: 5px; 
+    color: #333;
+    width: 250px; 
+}
+.goog-te-gadget {
+    color: transparent;
+    margin-bottom: -15px;
+}
+.goog-logo-link,.goog-te-banner-frame {
+    display: none;  
+    
+}
+body {
+    margin-top:0px!important; 
+    position: absolute; 
+    top: 0px!important
+}
+</style>
